825c6a13a9292698ba41814ff3c43a273dfa2438
byrnesy924 <b.byrnesy@gmail.com>
2024-01-13 00:30:52 +1100

    Load previous Bayesian results

--- HParameter_tuning_Bayesian_approach.py
+++ HParameter_tuning_Bayesian_approach.py
@@ -1,4 +1,5 @@
 import os
+import random
 import pandas as pd
 from time import perf_counter
 
@@ -32,19 +33,33 @@ def run_BO(BO_HParameters, number_epochs, load_results=False):
     optimizer = BO(  # TODO test the bounds minimiser object!
         f=test_model,
         pbounds=BO_HParameters,
-        verbose=2,
-        random_state=1
+        verbose=1,
+        random_state=random.randint(0, 20),
+        allow_duplicate_points=True  # Just easier to mark thi true as I'm piece-meal mapping the space
     )
 
-    if load_results:
-        load_logs(optimizer, path=[f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs.log"])
+    if load_results and os.path.isfile(f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs.log.json"):
+        # Create a backup with the appended file so as to not lose points when resetting
+        # This is necessary because the JSONlogger object only logs seen points and will overwrite the object when 
+        # initialised below
+        with open(f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs_backup.log.json", 'a') as backup:
+            with open(f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs.log.json", 'r') as original:
+                print(original.__dict__)
+                print(original.readlines)
+                backup.writelines(original.readlines())
+
+        load_logs(optimizer, logs=[f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs_backup.log.json"])
+        # See this - https://www.vidensanalytics.com/nouveau-blog/bayesian-optimization-to-the-rescue can load all logs 
+        # in a list!
+        print("Loaded previous points:\n")
+        print(optimizer.space, '\n', optimizer.res, '\n', optimizer.max)
 
     logger = JSONLogger(path=f"Results\\Hyperparameter_testing\\{number_epochs}_epochs\\BO_logs.log")
     optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
 
     optimizer.maximize(
-        init_points=10,
-        n_iter=15
+        init_points=5,
+        n_iter=60
     )
 
     print(optimizer.max)
@@ -53,7 +68,7 @@ def run_BO(BO_HParameters, number_epochs, load_results=False):
 
 
 pspace_GPy = [
-    {"name": "learning_rate", "type": "continuous", "domain":(0.000_000_1, 0.000_01)},
+    {"name": "learning_rate", "type": "continuous", "domain": (0.000_000_1, 0.000_01)},
     {"name": "gamma", "type": "continuous", "domain": (0.90, 0.99)},
     {"name": "reward_for_all_dice", "type": "continuous", "domain": (0, 10)},
     {"name": "reward_factor_for_initial_dice_picked", "type": "continuous", "domain": (0, 1)},
@@ -108,17 +123,21 @@ def run_GPyOPT(GPy_HParameters):
 
 
 if __name__ == "__main__":
+    # Set # epochs from run function
+    no_epochs = 128
+
     if not os.path.isdir("Results\\Hyperparameter_testing"):
         os.makedirs("Results\\Hyperparameter_testing")
+    if not os.path.isdir(f"Results\\Hyperparameter_testing\\{no_epochs}_epochs"):
+        os.makedirs(f"Results\\Hyperparameter_testing\\{no_epochs}_epochs")
 
     start = perf_counter()
-    no_epochs = 128
-    results = run_BO(BO_HParameters=pspace_BO, number_epochs=128, load_results=False)  # Make sure to change load
-    results.to_csv("Results\\Hyperparameter_testing\\{no_epochs}_epochs_bayesian_results")
+    results = run_BO(BO_HParameters=pspace_BO, number_epochs=128, load_results=True)  # Make sure to change load
+    results.to_csv(f"Results\\Hyperparameter_testing\\{no_epochs}_epochs\\Bayesian_results")
     BO_time = perf_counter()
 
     # TODO - check if I can use the knwoledge from the ranomd tests as a starting pad
-    print(f"BO took {round((BO_time - start)/60, 2)} mins to do method 5 times")
+    print(f"BO took {round((BO_time - start)/60, 2)} mins to do method 10 times")
 
     # run_GPyOPT(GPy_HParameters=pspace_GPy)  # Took ~ 60% longer and 3.11 + multiprocessing failed
     # print(f"GPy took {round((perf_counter() - BO_time)/60, 2)} mins to do method 5 times")
