4353793360c7276bc4f44ecd3d0ea2a8562ddc24
byrnesy924 <b.byrnesy@gmail.com>
2024-06-06 23:57:18 +1000

    altered HParameter tuning to do architecture

--- HParameter_tuning_Bayesian_approach.py
+++ HParameter_tuning_Bayesian_approach.py
@@ -27,33 +27,94 @@ from bayes_opt.event import Events
 from bayes_opt.util import load_logs
 
 # Import black box functions
-from Hparameter_tuning_random_approach import test_model
+# from Hparameter_tuning_random_approach import test_model  # 6 June updated to use function below
 from NNQmodel import NNQPlayer
 
 
-# Define paremter space
+def test_model(learning_rate=0.0008,
+               gamma=0.92,
+               structure_of_layers=32,
+               number_layers=3,
+               reward_for_all_dice=5,
+               reward_factor_for_initial_dice_picked=0.45,
+               reward_factor_total_score=3,
+               reward_factor_chosen_score=3.5,
+               reward_factor_for_picking_choice_correctly=5.2,
+               punish_amount_for_incorrect_score_choice=-3,
+               punish_for_not_picking_dice=-0.3,
+               batch_size=100,
+               memory=4800,
+               buffer_size=100,
+               name="HP_testing_architecture", random_results=False):
+    """ this is a black box wrapper function that trains the Q-Learning Network
+    """
+    model = NNQPlayer(
+        show_figures=False,
+        learning_rate=learning_rate,
+        gamma=gamma,
+        structure_of_layers=[structure_of_layers]*int(number_layers),
+        reward_for_all_dice=reward_for_all_dice,
+        reward_factor_total_score=reward_factor_total_score,
+        reward_factor_chosen_score=reward_factor_chosen_score,
+        reward_factor_for_initial_dice_picked=reward_factor_for_initial_dice_picked,
+        reward_factor_for_picking_choice_correctly=reward_factor_for_picking_choice_correctly,
+        punish_amount_for_incorrect_score_choice=punish_amount_for_incorrect_score_choice,
+        punish_factor_not_picking_dice=punish_for_not_picking_dice,
+        batch_size=int(batch_size),  # Note these need to be integers - coerce them to integers here for Bayesian Opt
+        length_of_memory=int(memory),
+        buffer_size=int(buffer_size),
+        name=name
+    )
+
+    model.run(16, 64, save_results=False, save_model=False, verbose=False)
+    results = [learning_rate,
+               gamma,
+               structure_of_layers,
+               number_layers,
+               reward_for_all_dice,
+               reward_factor_for_initial_dice_picked,
+               reward_factor_for_picking_choice_correctly,
+               batch_size,
+               memory,
+               buffer_size,
+               model.average_score,
+               model.average_loss
+               ]
+
+    # return results
+    # In this random script, we return a list of results for formatting into a DataFrame
+    # But in the Bayesian approach, we can only return a target variable, not a list
+    if random_results:
+        return results
+    return model.average_score
+
+
+# Define paremter space; 6 June - reduced curse of dimensionality by focusing on architecture
+
 pspace_BO = {
-    "batch_size": (32, 128),
-    "buffer_size": (32, 128),
-    "epochs": 12,
-    "gamma": (0.88, 0.99),
+    # "batch_size": 100,
+    # "buffer_size": 100,
+    # "epochs": 16,
+    # "gamma": 0.92,
     "learning_rate": (0.000_01, 0.001),
-    "memory": (2_000, 10_000),
-    "punish_amount_for_incorrect_score_choice": (-5, 0),
-    "punish_for_not_picking_dice": (-1, 0),
-    "reward_for_all_dice": (0, 10),
-    "reward_factor_for_initial_dice_picked": (0, 1),
-    "reward_factor_for_picking_choice_correctly": (0, 10),
+    # "memory": 4800,
+    "number_layers": (3, 6),
+    # "punish_amount_for_incorrect_score_choice": -3,
+    # "punish_for_not_picking_dice": -0.3,
+    # "reward_for_all_dice": 5,
+    # "reward_factor_for_initial_dice_picked": 0.45,
+    # "reward_factor_for_picking_choice_correctly": 5.2,
     "reward_factor_total_score": (0, 5),
-    "reward_factor_chosen_score": (0, 5)
+    # "reward_factor_chosen_score": 3.5,
+    "structure_of_layers": (16, 64)
 }
 
 
 def run_BO(BO_HParameters, number_epochs, init_runs, total_runs, load_results=False):
 
     # Included bounds transformer to dynamically change the bounds as the Bayesin agents search
-    bounds_transformer = SequentialDomainReductionTransformer(minimum_window=[1, 1, 0.1, 0.01, 0.00001, 100, 0.1, 0.1,
-                                                                              0.1, 0.1, 0.1, 0.1, 0.1])
+    bounds_transformer = SequentialDomainReductionTransformer(minimum_window=[0.000_01, 1, 1, 4])
+    # minimum_window=[1, 1, 1, 1, 0.000_01, 100, 1, 0.1, 0.1,0.1, 0.1, 0.1, 0.1, 0.1, 4]
 
     optimizer = BO(
         f=test_model,
@@ -61,24 +122,22 @@ def run_BO(BO_HParameters, number_epochs, init_runs, total_runs, load_results=Fa
         verbose=1,
         random_state=random.randint(0, 100),
         allow_duplicate_points=True,  # Just easier to mark this true as I'm piece-meal mapping the space
-        # bounds_transformer=bounds_transformer  # TODO get to work
+        bounds_transformer=bounds_transformer  # TODO get to work
     )
 
-    logs_backup = Path(f"Results/Hyperparameter_testing/{number_epochs}_epochs/BO_logs_backup.log.json")
-    logs_path = Path(f"Results/Hyperparameter_testing/{number_epochs}_epochs/BO_logs.log.json")
+    # logs_backup = Path(f"Results/Hyperparameter_testing/{number_epochs}_epochs/BO_logs_backup.log.json")
+    logs_path = Path(f"Results/Hyperparameter_testing/{number_epochs}_epochs/logs.log")
 
-    print(str(logs_backup))
+    # if load_results and os.path.isfile(logs_backup):
+    #     # Case where logs is not present but load is - then load the backup
+    #     print(str(logs_backup))
+    #     load_logs(optimizer, logs=[str(logs_backup)])
+    #     # See this - https://www.vidensanalytics.com/nouveau-blog/bayesian-optimization-to-the-rescue can load all logs
+    #     # in a list! this implimentation works fine but creating a new log each time may have been smarter
+    #     print("Loaded previous points:")
+    #     print('\n is max:\n', optimizer.max)
 
-    if load_results and os.path.isfile(logs_backup):
-        # Case where logs is not present but load is - then load the backup
-        print(str(logs_backup))
-        load_logs(optimizer, logs=[str(logs_backup)])
-        # See this - https://www.vidensanalytics.com/nouveau-blog/bayesian-optimization-to-the-rescue can load all logs
-        # in a list! this implimentation works fine but creating a new log each time may have been smarter
-        print("Loaded previous points:")
-        print('\n is max:\n', optimizer.max)
-
-    logger = JSONLogger(path=str(logs_path))
+    logger = JSONLogger(path=str(logs_path), reset=False)
     optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
 
     print("\nOptimising!\n")
@@ -310,25 +369,25 @@ def transfer_rows_to_backup(file_path: Path, backup_file_path: Path):
 
 
 if __name__ == "__main__":
-    no_epochs = pspace_BO["epochs"]
+    no_epochs = 16
     hparameter_testing = Path("Results/Hyperparameter_testing")
     epochs_path = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs")
     results_path = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs/Bayesian_results.csv")
 
-    load_results = True
-    if not os.path.isdir(hparameter_testing):
-        os.makedirs(hparameter_testing)
-    if not os.path.isdir(epochs_path):
-        os.makedirs(epochs_path)
-        load_results = False
+    # load_results = True
+    # if not os.path.isdir(hparameter_testing):
+    #     os.makedirs(hparameter_testing)
+    # if not os.path.isdir(epochs_path):
+    #     os.makedirs(epochs_path)
+    #     load_results = False
 
-    logs_path = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs/BO_logs.log.json")
-    logs_backup = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs/BO_logs_backup.log.json")
+    logs_path = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs/logs.log")
+    # logs_backup = Path(f"Results/Hyperparameter_testing/{no_epochs}_epochs/BO_logs_backup.log.json")
 
-    if load_results and os.path.isfile(logs_path):
-        # Create a backup with the appended files so as to not lose points when resetting
-        # This is necessary because the JSONlogger object only logs seen points and will overwrite the object when
-        transfer_rows_to_backup(logs_path, logs_backup)
+    # if load_results and os.path.isfile(logs_path):
+    #     # Create a backup with the appended files so as to not lose points when resetting
+    #     # This is necessary because the JSONlogger object only logs seen points and will overwrite the object when
+    #     transfer_rows_to_backup(logs_path, logs_backup)
 
     start = perf_counter()
 
@@ -337,7 +396,7 @@ if __name__ == "__main__":
                      number_epochs=no_epochs,
                      init_runs=1,
                      total_runs=0,
-                     load_results=load_results)
+                     load_results=True)
 
     # Multithreaded
     # no_processes = os.cpu_count() - 6
@@ -349,7 +408,7 @@ if __name__ == "__main__":
     results.to_csv(results_path, index=False)
     BO_time = perf_counter()
 
-    print(f"BO took {round((BO_time - start)/60, 2)} mins to do method {no_processes*4*20} times")
+    # print(f"BO took {round((BO_time - start)/60, 2)} mins to do method {no_processes*4*20} times")
 
     # run_GPyOPT(GPy_HParameters=pspace_GPy)  # Took ~ 60% longer and 3.11 + multiprocessing failed
     # print(f"GPy took {round((perf_counter() - BO_time)/60, 2)} mins to do method 5 times")
